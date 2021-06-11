87219eb64a81926171c39a7716bc62d8b689d19a
Elie Omatuku <elieomatuku@gmail.com>
2021-06-12 00:46:20 +0200

    Added ktlint & pre-commit

--- app/src/test/java/com/elieomatuku/weather_app/ExampleUnitTest.kt
+++ app/src/test/java/com/elieomatuku/weather_app/ExampleUnitTest.kt
@@ -2,7 +2,7 @@ package com.elieomatuku.weather_app
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
