20854e05285b1ca30d76ce1291980a5d54cd1ad5
Elie Omatuku <elieomatuku@gmail.com>
2021-06-14 00:30:00 +0200

    Fix

--- cache/src/main/java/com/elieomatuku/cache/weather/CachedWeather.kt
+++ cache/src/main/java/com/elieomatuku/cache/weather/CachedWeather.kt
@@ -1,6 +1,7 @@
 package com.elieomatuku.cache.weather
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 /**
  * Created by elieomatuku on 2021-06-12
@@ -8,6 +9,7 @@ import androidx.room.Entity
 
 @Entity
 data class CachedWeather(
+    @PrimaryKey val id: Int,
     val temperature: Double,
     val minimumTemperature: Double,
     val weatherConditionId: Long,
