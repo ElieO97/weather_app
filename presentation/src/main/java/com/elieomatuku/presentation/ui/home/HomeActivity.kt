2ec128452ae1afcf5c7096313083d46a696b849b
Elie Omatuku <elieomatuku@gmail.com>
2021-06-13 21:46:18 +0200

    More UI tweaks

--- presentation/src/main/java/com/elieomatuku/presentation/ui/home/HomeActivity.kt
+++ presentation/src/main/java/com/elieomatuku/presentation/ui/home/HomeActivity.kt
@@ -1,8 +1,11 @@
 package com.elieomatuku.presentation.ui.home
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
 import com.elieomatuku.presentation.R
 import com.elieomatuku.presentation.ui.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_home.*
 
 /**
  * Created by elieomatuku on 2021-06-13
@@ -12,5 +15,24 @@ class HomeActivity : BaseActivity(R.layout.activity_home) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_dehaze_24)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.title = null
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                if (drawerLayout.isDrawerOpen(navView)) {
+                    drawerLayout.close()
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START)
+                }
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
