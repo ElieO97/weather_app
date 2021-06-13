add6d307ad42f1d10914698adabb145123d161f8
Elie Omatuku <elieomatuku@gmail.com>
2021-06-13 18:16:20 +0200

    Base HomeActivity

--- presentation/src/main/java/com/elieomatuku/presentation/ui/home/HomeActivity.kt
+++ presentation/src/main/java/com/elieomatuku/presentation/ui/home/HomeActivity.kt
@@ -1,7 +1,12 @@
 package com.elieomatuku.presentation.ui.home
 
+import com.elieomatuku.presentation.R
+import com.elieomatuku.presentation.ui.base.BaseActivity
+
 /**
  * Created by elieomatuku on 2021-06-13
  */
 
-class HomeActivity
+class HomeActivity: BaseActivity(R.layout.activity_home) {
+
+}
