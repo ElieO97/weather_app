ca46dd5ff7162a729eefc1c8c8d24e948322ff26
Elie Omatuku <elieomatuku@gmail.com>
2021-06-13 19:03:28 +0200

    Base classes tweaks

--- presentation/src/main/java/com/elieomatuku/presentation/ui/base/BaseFragment.kt
+++ presentation/src/main/java/com/elieomatuku/presentation/ui/base/BaseFragment.kt
@@ -3,16 +3,25 @@ package com.elieomatuku.presentation.ui.base
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.elieomatuku.presentation.extensions.getSharedViewModel
+import com.elieomatuku.presentation.extensions.getViewModel
 import com.elieomatuku.presentation.extensions.lifecycleAwareLazy
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.closestKodein
+import org.kodein.di.generic.instance
 
 /**
  * Created by elieomatuku on 2021-06-13
  */
 
-class BaseFragment : Fragment {
+class BaseFragment : Fragment, KodeinAware {
 
     constructor()
     constructor(@LayoutRes resId: Int) : super(resId)
+
+    val viewModelFactory: ViewModelProvider.Factory by instance()
 
     protected inline fun <reified VM : ViewModel> getViewModel(): VM =
         getViewModel(viewModelFactory)
@@ -28,4 +37,6 @@ class BaseFragment : Fragment {
         lifecycleAwareLazy(this) {
             getSharedViewModel<VM>()
         }
+
+    override val kodein: Kodein by closestKodein(requireContext())
 }
--- presentation/src/main/java/com/elieomatuku/presentation/ui/home/HomeActivity.kt
+++ presentation/src/main/java/com/elieomatuku/presentation/ui/home/HomeActivity.kt
@@ -7,6 +7,4 @@ import com.elieomatuku.presentation.ui.base.BaseActivity
  * Created by elieomatuku on 2021-06-13
  */
 
-class HomeActivity: BaseActivity(R.layout.activity_home) {
-
-}
+class HomeActivity : BaseActivity(R.layout.activity_home)
